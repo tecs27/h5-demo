8ce23165cb34f96eedcd2574ed2ab0442f1d5dfe
aleche <aleche@tencent.com>
2025-08-11 18:41:33 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
